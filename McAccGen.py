24137768160e5c3161042a7de8e35685e6d1e676
RainbowRain <95581741+InvalidAccount69@users.noreply.github.com>
2022-05-25 07:21:29 -0700

    Update McAccGen.py

--- McAccGen.py
+++ McAccGen.py
@@ -1,47 +1,39 @@
-from random import choice
-from string import ascii_letters
-from threading import Thread
 import os
-
 try:
+    from random import choice
+    from string import ascii_letters
+    from threading import Thread
     from dhooks.client import Webhook
-except:
-    os.system("pip install dhooks")
-try:
     import colorama
-except:
-    os.system("pip install colorama")
-try:
     import requests
-except:
-    os.system("pip install requests")
+except ModuleNotFoundError:
+    os.system("pip install dhooks colorama requests")
     
 colorama.init()
-randString = lambda length: ''.join(choice(ascii_letters) for _ in range(length))
-webhook = "Your Webhook" # If Your Dont Have Webhook Dont Change It
-speed = 1 # Warning!
 
 
 
 
 class Main(Thread):
     def __init__(self):
+        self.randstr = lambda length: ''.join(choice(ascii_letters) for _ in range(length))
+        self.webhook = ""
         super().__init__(daemon=True)
 
     def run(self) -> None:
         while True:
-            st = randString(10)
-            with requests.get(f"http://bin.shortbin.eu:8080/documents/{st}") as s:
-                if s.status_code != 200:
-                    print(f"{colorama.Fore.RED}[-]  {st}")
+            String = self.randstr(10)
+            with requests.get(f"http://bin.shortbin.eu:8080/documents/{String}") as Request:
+                if Request.status_code != 200:
+                    print(f"{colorama.Fore.RED}[-]  {String}")
                     continue
 
-                data = s.json()["data"]
+                data = Request.json()["data"]
                 print(f"{colorama.Fore.LIGHTGREEN_EX}[+]  {data}")
                 
-                if webhook != "":
+                if self.webhook != "":
                     try:
-                        hook = Webhook(webhook)
+                        hook = Webhook(self.webhook)
                         hook.send(f"Account Scraped: `{data}`")
                     except:
                         pass
@@ -52,7 +44,7 @@ class Main(Thread):
                     f.write(data + "\n")
 
 
-for _ in range(speed):
+for _ in range(1): # 1 is Thread
     Main().start()
 while True:
     input()
