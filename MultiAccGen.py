91689517b16258a8f846e851ab5fc62c670b5bb2
BlackSnowDot <95581741+InvalidAccount69@users.noreply.github.com>
2022-09-24 07:46:59 -0700

    Update MultiAccGen.py

--- MultiAccGen.py
+++ MultiAccGen.py
@@ -1,14 +1,10 @@
 import os
-try:
-    from random import choice
-    from string import ascii_letters
-    from threading import Thread
-    from dhooks.client import Webhook
-    import colorama
-    import requests
-except ModuleNotFoundError:
-    os.system("pip install dhooks colorama requests")
-    
+from random import choice
+from string import ascii_letters
+from threading import Thread
+from dhooks.client import Webhook
+import colorama
+import requests
 colorama.init()
 
 
