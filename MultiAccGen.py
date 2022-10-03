02a2b169cf36fdd6081d84f33fa0d80a950a7979
BlackSnowDot <95581741+BlackSnowDot@users.noreply.github.com>
2022-10-03 13:30:55 -0700

    30 star

--- MultiAccGen.py
+++ MultiAccGen.py
@@ -1,45 +1,41 @@
+from contextlib import suppress
 from random import choice
 from string import ascii_letters
 from threading import Thread
 from dhooks.client import Webhook
 import colorama
-import requests
+from httpx import get
+from json import load
 
 colorama.init()
+config: dict = load(open('config.json'))
 
-## Config
-webhook = "" # your discord webhook here
-threads = 1 # higher = better chance of getting a account but requires a good pc
 
 class Main(Thread):
     def __init__(self):
         self.randstr = lambda length: ''.join(choice(ascii_letters) for _ in range(length))
         super().__init__(daemon=True)
 
-    def run(self) -> None:
+    def run(self):
         while True:
             string = self.randstr(10)
-            request = requests.get(f"http://bin.shortbin.eu:8080/raw/{string}")
+            request = get(f"http://bin.shortbin.eu:8080/raw/{string}", timeout=5)
             if request.status_code != 200:
-                print(f"{colorama.Fore.RED}[-] Failed | http://bin.shortbin.eu:8080/raw/{string} - {request.status_code} \n")
+                print(
+                    f"{colorama.Fore.RED}[-] Bad | http://bin.shortbin.eu:8080/raw/{string} - {request.status_code} \n")
                 continue
-                
-            print(f"{colorama.Fore.LIGHTGREEN_EX}[+] Hit | http://bin.shortbin.eu:8080/raw/{string} - {request.text} \n")
 
-            if webhook != "":
-                try:
-                    Webhook(webhook).send(f"Account Scraped: `{request.text}`")
-                except:
-                    pass
-            else:
-                pass
+            print(
+                f"{colorama.Fore.LIGHTGREEN_EX}[+] Hit | http://bin.shortbin.eu:8080/raw/{string} - {request.text} \n")
 
-            with open("results.txt", "a+") as f:
-                f.write(request.text + "\n")
+            if len(config['webhook']) > 100:
+                with suppress(Exception):
+                    Webhook(config['webhook']).send(f'{str(config["webhook_content"]).replace("<acc>", request.text)}')
+            open(config["accountFilePath"], "a+").write(request.text + "\n")
 
 
-for _ in range(threads):
-    Main().start()
-while True:
-    input()
- 
+if __name__ == '__main__':
+    for _ in range(config['thread']):
+        Main().start()
+    while True:
+        input()
